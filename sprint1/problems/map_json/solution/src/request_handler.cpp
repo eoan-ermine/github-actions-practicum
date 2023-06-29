4a25d993bc0cded18f77d92ee7981fe43b5d283f
PatriotRossii <patriotrossii2019@mail.ru>
2023-06-29 22:56:13 +0300

    [sprint1] Upload solution of map_json challenge

---
+++ sprint1/problems/map_json/solution/src/request_handler.cpp
@@ -0,0 +1,18 @@
+#include "request_handler.h"
+
+namespace request_handler {
+
+// Создаёт StringResponse с заданными параметрами
+StringResponse MakeStringResponse(http::status status, std::string_view body, unsigned http_version,
+                                  bool keep_alive,
+                                  std::string_view content_type) {
+    StringResponse response(status, http_version);
+    response.set(http::field::content_type, content_type);
+    response.body() = body;
+    response.content_length(body.size());
+    response.keep_alive(keep_alive);
+    return response;
+}
+
+
+}  // namespace request_handler
